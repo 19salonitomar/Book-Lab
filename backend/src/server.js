0bc94bf139b31da8026b02a692f5208fe6e53c65
Saloni Tomar <tomarsaloni19@gmail.com>
2026-01-23 02:37:21 +0530

    Added Server.js File

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,6 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
@@ -20,7 +19,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
