3adf0b5889aa1a673a30569b22d852a360b81768
Saloni Tomar <tomarsaloni19@gmail.com>
2026-01-23 18:34:40 +0530

    Added Server File

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,25 +5,34 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import path from "path";
-
+import fs from "fs";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+/* ---------- CREATE uploads FOLDER ---------- */
+const uploadDir = path.join(process.cwd(), "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+/* ---------- MIDDLEWARE ---------- */
 app.use(cors());
 app.use(express.json());
 
+/* ---------- ROUTES ---------- */
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(uploadDir));
 
 app.get("/", (req, res) => {
   res.send("BookLab API Running");
 });
 
+/* ---------- SERVER ---------- */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
