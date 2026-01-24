23ad508ceb55e2f5ed318ba988e7a205eb18aa66
Saloni Tomar <tomarsaloni19@gmail.com>
2026-01-24 22:01:27 +0530

    Remove local uploads, use Cloudinary only

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,11 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
-import path from "path";
-import fs from "fs";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-/* ---------- CREATE uploads FOLDER ---------- */
-const uploadDir = path.join(process.cwd(), "uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
 /* ---------- MIDDLEWARE ---------- */
 app.use(cors());
@@ -25,7 +17,6 @@ app.use(express.json());
 /* ---------- ROUTES ---------- */
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
-app.use("/uploads", express.static(uploadDir));
 
 app.get("/", (req, res) => {
   res.send("BookLab API Running");
